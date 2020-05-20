792e392ad0c119d351de7e0f0105070b75ad5ab4
John <example@gmail.com>
2020-05-20 17:38:48 +0200

    practice2

--- PracticeScript.js
+++ PracticeScript.js
@@ -10,12 +10,64 @@ const personalMovieDB = {
     privat: false
 };
 
-const ans1 = prompt('Один из последних просмотренных фильмов?',''),
-      ans2 = prompt('На сколько оцените его?',''),
-      ans3 = prompt('Один из последних просмотренных фильмов?',''),
-      ans4 = prompt('На сколько оцените его?','');
+// const ans1 = prompt('Один из последних просмотренных фильмов?',''),
+//       ans2 = prompt('На сколько оцените его?',''),
+//       ans3 = prompt('Один из последних просмотренных фильмов?',''),
+//       ans4 = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[ans1] = ans2;
-personalMovieDB.movies[ans3] = ans4;
+// personalMovieDB.movies[ans1] = ans2;
+// personalMovieDB.movies[ans3] = ans4;
 
+// for (let i = 0; i < 2; i++) {
+//     const ans1 = prompt('Один из последних просмотренных фильмов?',''),
+//           ans2 = prompt('На сколько оцените его?','');
+
+//     if (ans1 != null && ans2 != null && ans1 != '' && ans2 != '' && ans1.length < 50) {
+//         personalMovieDB.movies[ans1] = ans2;
+//         console.log('DONE');
+//     } else {
+//         console.log('error!');
+//         i--;
+//     }
+// }
+
+// let x =0;
+// while (x < 2) {
+//     const ans1 = prompt('Один из последних просмотренных фильмов?',''),
+//           ans2 = prompt('На сколько оцените его?','');
+
+//     if (ans1 != null && ans2 != null && ans1 != '' && ans2 != '' && ans1.length < 50) {
+//         personalMovieDB.movies[ans1] = ans2;
+//         console.log('DONE');
+//     } else {
+//         console.log('error!');
+//         x--;
+//     }
+//     x++;
+// }
+
+let x =0;
+do {
+    const ans1 = prompt('Один из последних просмотренных фильмов?',''),
+          ans2 = prompt('На сколько оцените его?','');
+
+    if (ans1 != null && ans2 != null && ans1 != '' && ans2 != '' && ans1.length < 50) {
+        personalMovieDB.movies[ans1] = ans2;
+        console.log('DONE');
+    } else {
+        console.log('error!');
+        x--;
+    }
+    x++;
+} while (x < 2);
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
